e7ebe9e286cd1e00d153d07bb12442789e5242ec
anthonykukoyi <63237420+anthonykukoyi@users.noreply.github.com>
2020-04-21 10:22:28 +0100

    files

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <h1 style="margin-top: 10px">Blog Posts</h1>
+    <br>
+        @if(count($posts) > 0)
+                @foreach($posts as $post)
+                    <a href="/posts/{{$post->id}}">
+                        <div class="card" style="margin-bottom:15px">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-4" style="margin-right:auto margin-left:auto">
+                                    <img class="card-img-top" src="/storage/cover_images/{{$post->cover_image}}">
+                                </div>
+                                <div class="col-md-8 col-sm-8">
+                                    <h3>{{$post->title}}</h3>
+                                    <small>Written on {{$post->created_at->format('d.m.Y H:i')}} by {{$post->user->name}}</small>
+                                </div>     
+                            </div>    
+                        </div>
+                    </a>
+                @endforeach
+                {{$posts->links()}}
+            @else
+            <p>No posts found</p>
+        @endif
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <a style="margin-top: 10px" href="/posts" class="btn btn-primary">Go Back</a>
+    <h1 style="margin-top: 5px">{{$post->title}}</h1>
+    <hr>
+        <small>Written on {{$post->created_at->format('d.m.Y H:i')}} by {{$post->user->name}}</small>
+    <hr>
+    <img style="width:75%" src="/storage/cover_images/{{$post->cover_image}}">
+    <br><br>                    
+    <div>
+         {!!$post->body!!}
+    </div>
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+        @endif
+    @endif
+@endsection
